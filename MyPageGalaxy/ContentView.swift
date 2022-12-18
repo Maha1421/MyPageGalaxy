fd43097362cfedecb15e8a2293c1d2204ae4334d
Maha <60257033+Maha1421@users.noreply.github.com>
2022-12-18 16:19:36 +0300

    Initial Commit

---
+++ MyPageGalaxy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyPageGalaxy
+//
+//  Created by Maha Saleh Alghamdi on 24/05/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyPageGalaxy/MyPageGalaxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPageGalaxyApp.swift
+//  MyPageGalaxy
+//
+//  Created by Maha Saleh Alghamdi on 24/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct MyPageGalaxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
